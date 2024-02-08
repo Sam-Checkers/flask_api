13d4478604973403e4a5a32b9740ed7a6fd1a3a5
Sam-Checkers <samcollins713@gmail.com>
2024-02-08 13:31:49 -0500

    first commit

---
+++ migrations/versions/698711cb1f74_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 698711cb1f74
+Revises: 
+Create Date: 2024-02-08 13:08:55.165141
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '698711cb1f74'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
